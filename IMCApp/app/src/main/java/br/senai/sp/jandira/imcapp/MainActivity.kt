8cf449b43f3f2dedcf4f128d30f487d85a781008
KAYLANE GUARINO SANTANA <20272558@cfp127aluno.local>
2021-03-05 11:23:32 -0300

    First Commit

---
+++ IMCApp/app/src/main/java/br/senai/sp/jandira/imcapp/MainActivity.kt
@@ -0,0 +1,33 @@
+package br.senai.sp.jandira.imcapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button_calcular.setOnClickListener {
+
+            calcularImc()
+
+        }
+
+    }
+
+    fun calcularImc() {
+        var peso = edit_peso.text.toString().toDouble()
+        var altura = edit_altura.text.toString().toDouble()
+
+        var imc = peso / (altura * altura)
+
+        text_resultado.text = imc.toString()
+
+        Toast.makeText(this,"Meu primeiro programa Android!", Toast.LENGTH_SHORT).show()
+
+    }
+
+}
